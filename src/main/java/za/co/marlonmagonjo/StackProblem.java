d3c54c297fde690baed7b49c6db99a5e6a1c06d4
marlonmilesmg <magonjomarlon@gmail.com>
2023-02-21 15:03:02 +0200

    Stack Problem

---
+++ src/main/java/za/co/marlonmagonjo/StackProblem.java
@@ -0,0 +1,15 @@
+package za.co.marlonmagonjo;
+
+class Stack {
+    private Integer[] arr;
+    private int top;
+    private int limit;
+
+    private static final int DEFAULT_LIMIT = 5;
+
+    public Stack(){
+
+    }
+}
+public class StackProblem {
+}
