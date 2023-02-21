d4693d48513f94b2eafc7d2aec94b1aa33ac5d50
marlonmilesmg <magonjomarlon@gmail.com>
2023-02-21 15:40:03 +0200

    Stack Problem with Exceptions, Optionals

--- src/main/java/za/co/marlonmagonjo/StackProblem.java
+++ src/main/java/za/co/marlonmagonjo/StackProblem.java
@@ -1,5 +1,7 @@
 package za.co.marlonmagonjo;
 
+import java.util.Optional;
+
 class Stack {
     private Integer[] arr;
     private int top;
@@ -8,8 +10,73 @@ class Stack {
     private static final int DEFAULT_LIMIT = 5;
 
     public Stack(){
+        initialize(DEFAULT_LIMIT);
+    }
 
+    public Stack(int size){
+        initialize(size);
+    }
+
+    private void initialize(int size){
+        arr = new Integer[size];
+        top = -1;
+        limit = size;
+    }
+
+    public void push(Integer ele) throws StackException{
+        if(top < limit - 1) {
+            arr[++top] = ele;
+        } else {
+            throw new StackException("Stack Overflow");
+        }
+    }
+
+    public Integer pop() throws StackException{
+        if(isEmpty()){
+            throw new StackException("Stack Empty");
+        }
+        return arr[top--];
+    }
+
+    public Optional<Integer> peek(){
+        if(isEmpty()){
+            return Optional.empty();
+        }
+        return Optional.of(arr[top]);
+    }
+
+    public boolean isEmpty(){
+        return top == -1;
+    }
+}
+
+class StackException extends Exception {
+    public StackException(String reason){
+        super(reason);
     }
 }
 public class StackProblem {
+
+    public static void main(String[] args) throws StackException{
+        Stack stk = new Stack(5);
+        stk.push(10);
+        stk.push(20);
+        stk.push(30);
+        stk.push(40);
+        // stk.push(50);
+        System.out.println(stk.pop());
+        System.out.println(stk.pop());
+        System.out.println(stk.pop());
+        System.out.println(stk.pop());
+        // System.out.println(stk.pop());
+        stk.push(45);
+
+        Optional<Integer> peekValue = stk.peek();
+        if(peekValue.isPresent()) {
+            System.out.println(peekValue.get());
+        }
+
+        System.out.println(stk.peek());
+        System.out.println(stk.pop());
+    }
 }
