f346dde36dd3e7af4945a19d8d4c85abb5d74921
marlonmilesmg <magonjomarlon@gmail.com>
2023-03-16 14:25:43 +0200

    Files - copy file contents from one file to the other using FileInputStream and FileOutputStream, Bytes, Buffer, Util

--- src/main/java/za/co/marlonmagonjo/FilesCopyContentsToAnotherFile.java
+++ src/main/java/za/co/marlonmagonjo/FilesCopyContentsToAnotherFile.java
@@ -1,10 +1,18 @@
 package za.co.marlonmagonjo;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
 
+class IOUtils{
+    public static void copy(OutputStream outputStream, InputStream inputStream) throws IOException{
+        byte [] buffer = new byte[1024];
+        int n;
+
+        // write buffer only bytes from 0 to n
+        while((n = inputStream.read(buffer)) > 0 ){
+            outputStream.write(buffer, 0, n);
+        }
+    }
+}
 public class FilesCopyContentsToAnotherFile {
     public static void main(String[] args) throws IOException{
         File baseDir = new File("C:/Users/ASA-2022/IdeaProjects/Daily_Coding_Practice_JAVA/src/main/resources");
@@ -33,12 +41,27 @@ public class FilesCopyContentsToAnotherFile {
         byte [] buffer = new byte[1024];
         int n;
 
-        // write buffer only bytes from 0 to n
+        // copy and write buffer only bytes from 0 to n
         while((n = fileInputStream1.read(buffer)) > 0 ){
             fileOutputStream1.write(buffer, 0, n);
         }
 
         fileInputStream1.close();
         fileInputStream1.close();
+
+        // copy and write using Util
+        File sourceFile2 = new File(baseDir, "e.txt");
+        File destinationFile2 = new File(baseDir, "f.txt");
+
+        FileInputStream fileInputStream2 = new FileInputStream(sourceFile2);
+        FileOutputStream fileOutputStream2 = new FileOutputStream(destinationFile2);
+
+        try {
+            IOUtils.copy(fileOutputStream2, fileInputStream2);
+        }
+        finally {
+            fileOutputStream2.close();
+            fileInputStream2.close();
+        }
     }
 }
