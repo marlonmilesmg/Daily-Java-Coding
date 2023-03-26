ed46844ef9fca7abe6d27b997cebb29753bc33e3
marlonmilesmg <magonjomarlon@gmail.com>
2023-03-26 21:04:40 +0200

    Hashing

--- src/main/java/za/co/marlonmagonjo/Hashing.java
+++ src/main/java/za/co/marlonmagonjo/Hashing.java
@@ -3,12 +3,44 @@ package za.co.marlonmagonjo;
 import java.util.HashSet;
 import java.util.Objects;
 
+/**
+ *  Collections - Prebuilt Data Structure Implementations
+ *
+ *  I - Interface
+ *  C - Class
+ *
+ *  Collection - (I)
+ *      List - (I)
+ *          Arraylist - (C)
+ *          LinkedList - (C)
+ *
+ *      Set - (I)
+ *          HashSet - (C)
+ *          LinkedHashSet - (C)
+ *          SortedSet - (I)
+ *          TreeSet - (C)
+ *
+ *      Map - (I)
+ *          HashMap - (C)
+ *          LinkedHashMap - (C)
+ *          SortedMap - (I)
+ *          TreeMap - (I)
+ *
+ *  List - Allows duplicates
+ *  Set - Does not allow duplicates
+ *  Hash - Uses hashing (insertion order not preserved)
+ *  Linked - Insertion order preserved
+ *  Tree - Sorted
+ *
+ */
+
 class Fruit{
     private String name;
     public Fruit(String name){
         this.name = name;
     }
 
+    // Override equals() and hashCode() to not allow duplicates in the HashSet
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
