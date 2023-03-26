f7071b2df6025561e2015f62e6b5eb9aefee373b
marlonmilesmg <magonjomarlon@gmail.com>
2023-03-26 21:05:15 +0200

    Implementation of Map

--- src/main/java/za/co/marlonmagonjo/Map.java
+++ src/main/java/za/co/marlonmagonjo/Map.java
@@ -2,6 +2,37 @@ package za.co.marlonmagonjo;
 
 import java.util.HashMap;
 
+/**
+ *  Collections - Prebuilt Data Structure Implementations
+ *
+ *  I - Interface
+ *  C - Class
+ *
+ *  Collection - (I)
+ *      List - (I)
+ *          Arraylist - (C)
+ *          LinkedList - (C)
+ *
+ *      Set - (I)
+ *          HashSet - (C)
+ *          LinkedHashSet - (C)
+ *          SortedSet - (I)
+ *          TreeSet - (C)
+ *
+ *      Map - (I)
+ *          HashMap - (C)
+ *          LinkedHashMap - (C)
+ *          SortedMap - (I)
+ *          TreeMap - (I)
+ *
+ *  List - Allows duplicates
+ *  Set - Does not allow duplicates
+ *  Hash - Uses hashing (insertion order not preserved)
+ *  Linked - Insertion order preserved
+ *  Tree - Sorted
+ *
+ */
+
 public class Map {
 
     public static void main(String[] args) {
