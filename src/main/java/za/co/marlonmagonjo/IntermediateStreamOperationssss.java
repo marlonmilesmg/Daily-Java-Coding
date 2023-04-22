0af18cc4be708d9ec736d2c13b80e7b170d68fe5
marlonmilesmg <magonjomarlon@gmail.com>
2023-04-22 19:55:45 +0200

    Streams Intermediate Operations -> sorted, sorted(Comparator)

---
+++ src/main/java/za/co/marlonmagonjo/IntermediateStreamOperationssss.java
@@ -0,0 +1,29 @@
+package za.co.marlonmagonjo;
+
+import java.util.Arrays;
+
+/**
+ *  Intermediate Operations -
+ *      Sorting streams
+ *          - sorted(Comparator) - returns a stream with elements sorted based on comparator
+ *          - sorted() - return stream sorted using natural order
+ */
+
+public class IntermediateStreamOperationssss {
+    public static void main(String[] args) {
+        String [] arr = {"ab", "a", "xy", "bc"};
+
+        System.out.println("Sorting using natural order");
+        Arrays.stream(arr)
+                .sorted()
+                .forEach(System.out::println);
+        System.out.println("=======================================");
+
+        System.out.println("Sorting using comparator");
+        Arrays.stream(arr)
+                .sorted((x, y) -> -1 * x.compareTo(y))
+                .forEach(System.out::println);
+        System.out.println("=======================================");
+
+    }
+}
