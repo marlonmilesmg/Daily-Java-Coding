7069f5e5cd8dc8d4ff9e7d7c868395b51b773298
marlonmilesmg <magonjomarlon@gmail.com>
2023-05-02 15:04:01 +0200

    Method Overloading

--- src/main/java/za/co/marlonmagonjo/MethodOverloading.java
+++ src/main/java/za/co/marlonmagonjo/MethodOverloading.java
@@ -1,7 +1,28 @@
 package za.co.marlonmagonjo;
 
+/**
+ *  Overloading -
+ *      Providing multiple definitions to the same identifier
+ */
+
+class Saample{
+    void print(int a){
+        System.out.println("int - " + a);
+    }
+
+    void print(float a){
+        System.out.println("float - " + a);
+    }
+
+    void print(String a){
+        System.out.println("String - "+ a);
+    }
+}
 public class MethodOverloading {
     public static void main(String[] args) {
-
+        Saample s1 = new Saample();
+        s1.print(10);
+        s1.print(12.4f);
+        s1.print("ASDF");
     }
 }
