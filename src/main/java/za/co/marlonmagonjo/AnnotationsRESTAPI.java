849c1d5bce4436366ffdca98232ed569cfd9d718
marlonmilesmg <magonjomarlon@gmail.com>
2023-05-08 21:06:02 +0200

    REST API Annotations

---
+++ src/main/java/za/co/marlonmagonjo/AnnotationsRESTAPI.java
@@ -0,0 +1,32 @@
+package za.co.marlonmagonjo;
+
+/**
+ *
+ * @RestController - is a specialized version of the controller.
+ * It includes the @Controller and @ResponseBody annotations, and as a result, simplifies the controller implementation.
+ * The controller is annotated with the @RestController annotation; therefore, the @ResponseBody isn't required.
+ * Every request handling method of the controller class automatically serializes return objects into HttpResponse.
+ *
+ * @RequestMapping - the annotation is used to map web requests to Spring Controller methods.
+ *
+ * @GetMapping
+ *
+ * @PostMapping
+ *
+ * @PutMapping
+ *
+ * @DeleteMapping
+ *
+ * @RequestBody
+ *
+ * @PathVariable
+ *
+ * @RequestParam
+ *
+ * @ControllerAdvice
+ *
+ * @ExceptionHandler
+ *
+ */
+public class AnnotationsRESTAPI {
+}
