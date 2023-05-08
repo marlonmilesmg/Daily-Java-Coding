5546b95973403f59951d5b50b3311fed432b285e
marlonmilesmg <magonjomarlon@gmail.com>
2023-05-08 21:07:25 +0200

    Spring Core Annotations

---
+++ src/main/java/za/co/marlonmagonjo/AnnotationsSpringCore.java
@@ -0,0 +1,27 @@
+package za.co.marlonmagonjo;
+
+/**
+ *
+ * @Configuration
+ *
+ * @Bean
+ *
+ * @Autowired
+ *
+ * @Qualifier
+ *
+ * @Lazy
+ *
+ * @Value
+ *
+ * @PropertySource
+ *
+ * @ConfigurationProperties
+ *
+ * @Profile
+ *
+ * @Scope
+ *
+ */
+public class AnnotationsSpringCore {
+}
