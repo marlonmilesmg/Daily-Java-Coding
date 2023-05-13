757eb4053774049bf8f6ce10c54d8e8d9ed653d0
marlonmilesmg <magonjomarlon@gmail.com>
2023-05-13 21:31:33 +0200

    Abstract Classes

---
+++ src/main/java/za/co/marlonmagonjo/AbstractClasses.java
@@ -0,0 +1,21 @@
+package za.co.marlonmagonjo;
+
+/**
+ *
+ *  Abstract classes -
+ *      - Cannot be instantiated
+ *      - They are declared abstract and may or may not include abstract methods
+ *      - They can be subclassed
+ *      - Abstract classes and abstract methods cannot be final
+ *
+ *  Concrete classes -
+ *      - Classes which can be instantiated
+ *
+ *
+ */
+
+public class AbstractClasses {
+    public static void main(String[] args) {
+
+    }
+}
