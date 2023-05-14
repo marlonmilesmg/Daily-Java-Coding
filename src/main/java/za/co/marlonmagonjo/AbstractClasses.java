efa1cac92c8a98787ae7df68b497ef2db023bde1
marlonmilesmg <magonjomarlon@gmail.com>
2023-05-14 20:05:24 +0200

    Abstract Classes

--- src/main/java/za/co/marlonmagonjo/AbstractClasses.java
+++ src/main/java/za/co/marlonmagonjo/AbstractClasses.java
@@ -7,6 +7,7 @@ package za.co.marlonmagonjo;
  *      - They are declared abstract and may or may not include abstract methods
  *      - They can be subclassed
  *      - Abstract classes and abstract methods cannot be final
+ *      - Whenever you have an abstract method, the corresponding class should be abstract
  *
  *  Concrete classes -
  *      - Classes which can be instantiated
@@ -14,8 +15,32 @@ package za.co.marlonmagonjo;
  *
  */
 
+abstract class Basee {
+    public void f(){
+        System.out.println("f() in Basee");
+    }
+
+    public abstract void g();
+}
+
+class Deriveed extends Basee {
+
+    @Override
+    public void g() {
+        System.out.println("g() in Deriveed");
+    }
+}
+
 public class AbstractClasses {
     public static void main(String[] args) {
+        Basee b;
+        b = new Deriveed();
+        b.f();
+        b.g();
+        System.out.println();
 
+        Deriveed d = new Deriveed();
+        d.f();
+        d.f();
     }
 }
