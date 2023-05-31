b87569c72a6c2ac9ff610037a36010c0b991eea4
marlon <magonjomarlon@gmail.com>
2023-06-01 00:02:57 +0200

    Interfaces - Cohesion, Coupling

---
+++ src/main/java/za/co/marlonmagonjo/Interfacessssss.java
@@ -0,0 +1,19 @@
+package za.co.marlonmagonjo;
+
+/**
+ *
+ *  Cohesion - Defines interaction between objects
+ *      Low cohesion - BAD
+ *      High cohesion - GOOD
+ *  Coupling - Dependency among objects
+ *      Tight coupling - BAD
+ *      Loose coupling - GOOD
+ *  Interfaces for loosely coupled code
+ *
+ */
+
+public class Interfacessssss {
+    public static void main(String[] args) {
+
+    }
+}
