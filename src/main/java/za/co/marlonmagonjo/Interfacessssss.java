1b321c42751c9157394a83124e8daf4d46b8fd24
marlon <magonjomarlon@gmail.com>
2023-06-03 23:16:59 +0200

    Interfaces - Cohesion, Coupling

--- src/main/java/za/co/marlonmagonjo/Interfacessssss.java
+++ src/main/java/za/co/marlonmagonjo/Interfacessssss.java
@@ -1,5 +1,7 @@
 package za.co.marlonmagonjo;
 
+import java.util.Scanner;
+
 /**
  *
  *  Cohesion - Defines interaction between objects
@@ -12,8 +14,49 @@ package za.co.marlonmagonjo;
  *
  */
 
+interface VideoPlayerr {
+    void play(String file);
+}
+
+class MP4Playerr implements VideoPlayerr {
+
+    @Override
+    public void play(String file) {
+        System.out.println("MP4Player playing the file - " + file);
+    }
+}
+
+class MOVPlayerr implements VideoPlayerr {
+
+    @Override
+    public void play(String file) {
+        System.out.println("MOVPlayer playing the file - " + file);
+    }
+}
+
+class PlayerFactoryy {
+    public static VideoPlayerr getPlayer(String file){
+        if(file.endsWith("mp4")){
+            return new MP4Playerr();
+        }
+        if(file.endsWith("mov")){
+            return new MOVPlayerr();
+        }
+        return null;
+    }
+}
+
 public class Interfacessssss {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Which file do you want to play");
+        String file = scan.nextLine();
 
+        VideoPlayerr player = PlayerFactoryy.getPlayer(file);
+        if (player == null){
+            System.out.println("Could not find a player for the file");
+        } else {
+            player.play(file);
+        }
     }
 }
