ef3336d3c77e24f330e16f1dbd13fad8e10f18b6
marlon <magonjomarlon@gmail.com>
2023-06-04 22:46:06 +0200

    Packages

--- src/main/java/za/co/marlonmagonjo/Packages.java
+++ src/main/java/za/co/marlonmagonjo/Packages.java
@@ -1,5 +1,24 @@
 package za.co.marlonmagonjo;
 
+/**
+ *
+ *  Packages - normal naming convention
+ *
+ *   Access modifier for class
+ *      - default: accessible only within the package
+ *      - public: accessible outside of package
+ *
+ *   Access modifier for members-
+ *      - private: accessible only within that class
+ *      - default: package level access
+ *      - protected: package level + derived classes
+ *      - public: accessible
+ *
+ *  Outside Packages
+ *      - Sub classes: have access to protected and public
+ *
+ */
+
 public class Packages {
     public static void main(String[] args) {
 
