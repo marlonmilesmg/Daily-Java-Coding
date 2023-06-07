3b793b0ee4f754126445118cf8324a51e403a7c9
marlon <magonjomarlon@gmail.com>
2023-06-07 11:31:26 +0200

    Servlets

---
+++ src/main/java/za/co/marlonmagonjo/questionandanswer/JEE.java
@@ -0,0 +1,17 @@
+package za.co.marlonmagonjo.questionandanswer;
+
+/**
+ *  Servlets
+ *      HTTP-specific servlets allow developers to process data submitted by HTML forms. Servlets are Java classes that run on a web server and handle requests and responses in the HTTP protocol.
+ *      When an HTML form is submitted, the data entered by the user is sent to the server as an HTTP request. Servlets can be used to receive and process this data.
+ *      The servlet retrieves the form data from the request object and performs the necessary operations, such as storing the data in a database, performing calculations, or generating a response.
+ *      Servlets provide methods that allow developers to access form data easily. For example, the doPost() method can be used to handle HTTP POST requests, which are commonly used for submitting form data.
+ *      The servlet can extract the submitted data using methods like getParameter() to retrieve individual form fields or getParameterValues() to retrieve multiple values from checkboxes or select lists.
+ *      By using servlets, developers can receive and process data submitted by HTML forms, perform server-side operations, and generate appropriate responses to the user.
+ *
+ */
+public class JEE {
+    public static void main(String[] args) {
+
+    }
+}
