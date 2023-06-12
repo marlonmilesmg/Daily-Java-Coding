5710b10c48170100afa08c74076bc29da1585506
Marlon Magonjo <39700735+marlonmilesmg@users.noreply.github.com>
2023-06-12 15:04:48 +0200

    Update JEE Questions and Answer

--- src/main/java/za/co/marlonmagonjo/questionandanswer/JEE.java
+++ src/main/java/za/co/marlonmagonjo/questionandanswer/JEE.java
@@ -9,6 +9,42 @@ package za.co.marlonmagonjo.questionandanswer;
  *      The servlet can extract the submitted data using methods like getParameter() to retrieve individual form fields or getParameterValues() to retrieve multiple values from checkboxes or select lists.
  *      By using servlets, developers can receive and process data submitted by HTML forms, perform server-side operations, and generate appropriate responses to the user.
  *
+ *  What is Java EE and what are its key components?
+ *      Java EE (Java Enterprise Edition) is a platform that provides a collection of specifications and APIs for developing enterprise-level, distributed, and scalable applications in Java. 
+ *      It offers a robust and standardized framework for building enterprise software systems.
+ *
+ *      The key components of Java EE include:
+ *
+ *      1. Servlet: It is a Java class that handles HTTP requests and responses, providing a foundation for web application development.
+ *
+ *      2. JavaServer Pages (JSP): It is a technology that allows developers to embed Java code within HTML pages, facilitating dynamic web page generation.
+ *
+ *      3. JavaServer Faces (JSF): It is a component-based web framework that simplifies the development of user interfaces for web applications.
+ *
+ *      4. Enterprise JavaBeans (EJB): It is a server-side component model that enables the development of scalable and transactional business logic in enterprise applications. 
+ *         EJBs can be of three types: session beans, entity beans, and message-driven beans.
+ *
+ *      5. Java Persistence API (JPA): It is a specification that provides a standardized way to manage persistent data using Object-Relational Mapping (ORM) techniques. 
+ *         JPA simplifies the interaction between Java objects and relational databases.
+ *
+ *      6. Java Message Service (JMS): It is an API that enables Java applications to send, receive, and process messages asynchronously, promoting loose coupling and reliable communication between distributed components.
+ *
+ *      7. Java Transaction API (JTA): It provides a standard interface for managing distributed transactions across multiple resources, ensuring data consistency and integrity.
+ *
+ *      8. Java Naming and Directory Interface (JNDI): It allows Java applications to access naming and directory services, such as LDAP, to locate and access various resources, such as databases and messaging services.
+ *
+ *      9. JavaMail: It is an API for sending and receiving email messages using Java.
+ *
+ *      10. Java Authentication and Authorization Service (JAAS): It provides a framework for user authentication and authorization in Java applications, supporting various authentication mechanisms and access control policies.
+ *
+ *      These components work together to provide a comprehensive and standardized platform for building enterprise applications in Java.
+ *
+ *
+ *
+ *
+ *
+ *
+ *
  */
 public class JEE {
     public static void main(String[] args) {
