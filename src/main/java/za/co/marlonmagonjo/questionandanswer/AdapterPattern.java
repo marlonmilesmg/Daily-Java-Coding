3eebfed335d77d8901332af6a88d5ca14918dc8c
marlon <magonjomarlon@gmail.com>
2023-06-30 23:39:41 +0200

    Adapter Pattern

---
+++ src/main/java/za/co/marlonmagonjo/questionandanswer/AdapterPattern.java
@@ -0,0 +1,12 @@
+package za.co.marlonmagonjo.questionandanswer;
+
+/**
+ *  Adapter pattern is a behavioral design pattern.
+ *  It is used to convert the interface of one class into another interface that clients expect.
+ *  This pattern allows classes with incompatible interfaces to work together by creating a bridge between them.
+ *  The Adapter pattern is often used when integrating new or existing code that cannot be directly modified to meet the interface requirements of other code.
+ *  By using an adapter, the two incompatible interfaces can communicate and interact with each other.
+ *
+ */
+public class AdapterPattern {
+}
