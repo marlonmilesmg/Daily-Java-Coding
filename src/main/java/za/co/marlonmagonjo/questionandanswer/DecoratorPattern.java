feb3130dabaa63c6b6d12e0d68352c6aa464fec0
marlon <magonjomarlon@gmail.com>
2023-06-30 23:43:38 +0200

    Decorator Pattern

---
+++ src/main/java/za/co/marlonmagonjo/questionandanswer/DecoratorPattern.java
@@ -0,0 +1,10 @@
+package za.co.marlonmagonjo.questionandanswer;
+
+/**
+ *  Decorator pattern is indeed a behavioral design pattern.
+ *  The decorator pattern allows you to dynamically add or modify behavior of an object at runtime by wrapping it with one or more decorator objects.
+ *  It is part of the behavioral design pattern category because it focuses on defining how objects communicate and interact with each other to achieve a desired behavior.
+ *
+ */
+public class DecoratorPattern {
+}
