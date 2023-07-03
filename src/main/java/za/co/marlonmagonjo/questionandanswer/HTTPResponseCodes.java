434793edb142f4fa0837e2317018fc96ffe2bc57
marlon <magonjomarlon@gmail.com>
2023-07-03 17:27:54 +0200

    HTTP Response Codes

---
+++ src/main/java/za/co/marlonmagonjo/questionandanswer/HTTPResponseCodes.java
@@ -0,0 +1,53 @@
+package za.co.marlonmagonjo.questionandanswer;
+
+/**
+ *
+ * A list of some common HTTP response codes:
+ * <p>
+     * 1xx Informational:
+ * <p>
+     * 100 Continue
+     * 101 Switching Protocols
+     * 102 Processing
+ * <p>
+     * 2xx Success:
+ * <p>
+     * 200 OK
+     * 201 Created
+     * 202 Accepted
+     * 204 No Content
+     * 206 Partial Content
+ * <p>
+     * 3xx Redirection:
+ * <p>
+     * 300 Multiple Choices
+     * 301 Moved Permanently
+     * 302 Found
+     * 304 Not Modified
+     * 307 Temporary Redirect
+     * 308 Permanent Redirect
+ * <p>
+     * 4xx Client Errors:
+ * <p>
+     * 400 Bad Request
+     * 401 Unauthorized
+     * 403 Forbidden
+     * 404 Not Found
+     * 405 Method Not Allowed
+     * 408 Request Timeout
+     * 409 Conflict
+     * 410 Gone
+     * 429 Too Many Requests
+ * <p>
+     * 5xx Server Errors:
+ * <p>
+     * 500 Internal Server Error
+     * 501 Not Implemented
+     * 502 Bad Gateway
+     * 503 Service Unavailable
+     * 504 Gateway Timeout
+     * 505 HTTP Version Not Supported
+ *
+ */
+public class HTTPResponseCodes {
+}
