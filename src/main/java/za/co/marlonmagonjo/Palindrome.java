be871cd2f546b56b664ecd95edf9f408ffe6c645
marlon <magonjomarlon@gmail.com>
2023-07-10 00:14:03 +0200

    Palindrome Problem

---
+++ src/main/java/za/co/marlonmagonjo/Palindrome.java
@@ -0,0 +1,28 @@
+package za.co.marlonmagonjo;
+
+public class Palindrome {
+
+    public static boolean isPalindrome(String str) {
+        int left = 0;
+        int right = str.length() - 1;
+
+        while (left < right) {
+            if (str.charAt(left) != str.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String str = "madam";
+
+        if (isPalindrome(str)) {
+            System.out.println(str + " is a palindrome.");
+        } else {
+            System.out.println(str + " is not a palindrome.");
+        }
+    }
+}
