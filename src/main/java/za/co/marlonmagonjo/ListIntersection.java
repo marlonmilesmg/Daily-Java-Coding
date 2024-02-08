98e94136e28caf3d38b4cd8cace3826f30bb7ff6
marlon <magonjomarlon@gmail.com>
2024-02-08 09:22:11 +0200

    Find the intersection of two lists - time complexity included

--- src/main/java/za/co/marlonmagonjo/ListIntersection.java
+++ src/main/java/za/co/marlonmagonjo/ListIntersection.java
@@ -6,6 +6,19 @@ import java.util.List;
 import java.util.Set;
 
 public class ListIntersection {
+    /**
+     * The time complexity of the given intersection method is O(min(n, m)),
+     * where n and m are the sizes of the input lists first and second, respectively.
+     * <p>
+     * "min" refers to the minimum operation. In the expression "O(min(n, m)),"
+     * it means that the time complexity of the algorithm is determined by the minimum size between the two input lists.
+     * <p>
+     *  Here's the breakdown:
+     *  - Creating HashSet from first: O(n)
+     *  - Creating HashSet from second: O(m)
+     *  - Finding the intersection using retainAll: O(min(n, m))
+     *  - Creating ArrayList from the result set: O(min(n, m))
+     */
 
     public static List<Integer> intersection(List<Integer> first, List<Integer> second) {
         // Create sets from the lists to find the intersection
