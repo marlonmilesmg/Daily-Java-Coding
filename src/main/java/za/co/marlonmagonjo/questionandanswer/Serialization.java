6462a99edde765a7c06c11091ce69a4c4d2f6253
marlon <magonjomarlon@gmail.com>
2024-02-26 09:26:50 +0200

    Serialization in Java

---
+++ src/main/java/za/co/marlonmagonjo/questionandanswer/Serialization.java
@@ -0,0 +1,9 @@
+package za.co.marlonmagonjo.questionandanswer;
+
+public class Serialization {
+    /**
+     * Serialization in Java is the concept of representing an object's state as a byte stream.
+     * The byte stream has all the information about the object. Usually used in Hibernate, JMS, JPA, and EJB,
+     * serialization in Java helps transport the code from one JVM to another and then de-serialize it there.
+     */
+}
