b35ae6aa31d7022ad5d1fd32439a52c4dcd12a14
marlonmagonjo1 <marlon.magonjo@hellogroup.co.za>
2024-11-14 10:34:59 +0200

    Strings

---
+++ src/main/java/za/co/marlonmagonjo/questionandanswer/Test6.java
@@ -0,0 +1,9 @@
+package za.co.marlonmagonjo.questionandanswer;
+
+public class Test6 {
+    public static void main(String[] args) {
+        String str1 = new String("Core");
+        String str2 = new String("CoRe");
+        System.out.println(str1 = str2);
+    }
+}
