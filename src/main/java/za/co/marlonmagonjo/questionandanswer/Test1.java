df8eff365373da063a1619f24db232e69f5ce80a
marlonmagonjo1 <marlon.magonjo@hellogroup.co.za>
2024-11-14 10:02:05 +0200

    try catch

---
+++ src/main/java/za/co/marlonmagonjo/questionandanswer/Test1.java
@@ -0,0 +1,12 @@
+package za.co.marlonmagonjo.questionandanswer;
+
+public class Test1 {
+    public static void main(String[] args) {
+        try {
+            main(args);
+        } catch (Exception ex) {
+            System.out.println("CATCH-");
+        }
+        System.out.println("OUT");
+    }
+}
