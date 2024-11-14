673a0f836770d4c25acdab3db7ecdf7ae8a56626
marlonmagonjo1 <marlon.magonjo@hellogroup.co.za>
2024-11-14 10:34:40 +0200

    Data Types

---
+++ src/main/java/za/co/marlonmagonjo/questionandanswer/Test5.java
@@ -0,0 +1,14 @@
+package za.co.marlonmagonjo.questionandanswer;
+
+public class Test5 {
+  char var1;
+  double var2;
+  float var3;
+
+  public static void main(String[] args) {
+    Test5 obj = new Test5();
+    System.out.println(">" + obj.var1);
+    System.out.println(">" + obj.var2);
+    System.out.println(">" + obj.var3);
+  }
+}
