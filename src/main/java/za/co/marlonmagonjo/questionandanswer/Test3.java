e737668858ea058f784cad20d2c03175bb7e1342
marlonmagonjo1 <marlon.magonjo@hellogroup.co.za>
2024-11-14 10:06:09 +0200

    Double

---
+++ src/main/java/za/co/marlonmagonjo/questionandanswer/Test3.java
@@ -0,0 +1,8 @@
+package za.co.marlonmagonjo.questionandanswer;
+
+public class Test3 {
+    public static void main(String[] args) {
+        Double [] arr = new Double[2];
+        System.out.println(arr[0] + arr[1]);
+    }
+}
